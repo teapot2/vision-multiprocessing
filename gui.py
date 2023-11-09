4023e69baa69ae98d11454f643ff710c50df6f7c
teapot2 <A01197460@itesm.mx>
2023-11-08 23:56:44 -0600

    Improved GUI styling

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
-import tkinter as tk
+from ttkthemes import ThemedTk
 from tkinter import ttk
+import tkinter as tk
 import time
 
 
@@ -24,27 +25,27 @@ def process_status_gui(shared_dict, monitor):
                 widget.destroy()
 
             header_labels = [
-                "process_id",
-                "camera_name",
-                "stream_name",
-                "execution_time",
-                "faces_detected",
+                "ID",
+                "Camera Name",
+                "Stream Name",
+                "Execution Time",
+                "Faces Detected",
             ]
 
             for idx, label in enumerate(header_labels):
-                ttk.Label(frame, text=label, font=("Helvetica", 10, "bold")).grid(
+                ttk.Label(frame, text=label, font=("Arial", 12, "bold")).grid(
                     row=0, column=idx, padx=5, pady=5
                 )
 
             row = 1
             for key, value in shared_dict.items():
                 ttk.Label(frame, text=str(key)).grid(row=row, column=0, padx=5, pady=5)
-                ttk.Label(frame, text=value["camera_name"]).grid(
-                    row=row, column=1, padx=5, pady=5
-                )
-                ttk.Label(frame, text=value["stream_name"]).grid(
-                    row=row, column=2, padx=5, pady=5
-                )
+                ttk.Label(
+                    frame, text=value["camera_name"], anchor="w", justify="left"
+                ).grid(row=row, column=1, padx=5, pady=5)
+                ttk.Label(
+                    frame, text=value["stream_name"], anchor="w", justify="left"
+                ).grid(row=row, column=2, padx=5, pady=5)
                 ttk.Label(frame, text=value["execution_time"]).grid(
                     row=row, column=3, padx=5, pady=5
                 )
@@ -55,12 +56,13 @@ def process_status_gui(shared_dict, monitor):
 
             root.after(100, update_gui)  # Update every 100 milliseconds
 
-    root = tk.Tk()
+    root = ThemedTk(theme="")
     root.title("Camera Process Status")
-    root.geometry("800x400")
 
     frame = ttk.Frame(root, padding=(5, 5, 5, 5))
     frame.grid(row=0, column=0, sticky="nsew")
+
+    frame.pack_propagate(0)  # Disable propagation
 
     update_gui()
 
